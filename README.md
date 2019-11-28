c7391efeb7072d622b94051f8e5d866c9d83fbf7
Krzysztof <hrabina.potocka@gmail.com>
2019-11-28 19:23:49 +0100

    atlas_conf_2019_040

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # current
 
+2019-11-28   Krzysztof Rolbiecki <krolb@fuw.edu.pl>
+        
+    ~ added atlas_conf_2019_040
+
 2019-11-22   Krzysztof Rolbiecki <krolb@fuw.edu.pl>
         
     ~ added atlas_1909_08457
--- tools/fritz/src/analysishandler/AnalysisHandlerATLAS_13TeV.cc
+++ tools/fritz/src/analysishandler/AnalysisHandlerATLAS_13TeV.cc
@@ -89,6 +89,8 @@ void AnalysisHandlerATLAS_13TeV::bookAnalysis(std::string analysisName,
         a = new Atlas_1908_08215();
     else if(analysisName == "atlas_1909_08457")
         a = new Atlas_1909_08457();
+    else if(analysisName == "atlas_conf_2019_040")
+        a = new Atlas_conf_2019_040();
     else //@@extracode@@
         Global::abort(name,
                       "Cannot load analysis "+analysisName+
