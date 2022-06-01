35b5f8e1b5a5a4a563e80aa30ce94b9e5a48ab0a
K. Rolbiecki <krolb@fuw.edu.pl>
2022-06-01 17:08:04 +0200

    atlas_2202_07953 v 1.0

--- tools/analysis/include/analyses/ATLAS_13TeV/atlas_2202_07953.h
+++ tools/analysis/include/analyses/ATLAS_13TeV/atlas_2202_07953.h
@@ -14,6 +14,13 @@ class Atlas_2202_07953 : public AnalysisBase {
     void finalize();
 
   private:
+    bool check_nTrack_jet(Jet* jet, std::vector<Track*> tracks, int nTracksMin) ;  
+    std::vector<Jet*> overlapRemoval_muon_jet_tracks(std::vector<Jet*> cand_jets, std::vector<Muon*> cand_muons, double deltaR, int nTracks);  
+    template <class X, class Y>
+    std::vector<X*> specialoverlap(std::vector<X*> candidates, std::vector<Y*> neighbours);    
+    template<class T> std::vector<T*> isolateLeptons(std::vector<T*> leptons, double deltaR);
+    std::vector<Photon*> isolatePhotons(std::vector<Photon*> photons, double deltaR);    
+    
 };
 
 #endif
--- tools/analysis/src/analyses/ATLAS_13TeV/atlas_2202_07953.cc
+++ tools/analysis/src/analyses/ATLAS_13TeV/atlas_2202_07953.cc
@@ -15,51 +15,243 @@ void Atlas_2202_07953::initialize() {
 }
 
 void Atlas_2202_07953::analyze() {
-  // Your eventwise analysis code goes here
-  // The following objects are always defined unless they are 'ignored' above. They form std::vector objects of the respective Delphes class type (except for Etmiss which is a single object)
-  // All std::vector members and etmiss have the common properties PT, Eta, Phi and P4() with the latter giving access to the full ROOT TLorentzVector.
-  // Within a std::vector, all members are ordered with highest pt coming first.
-
-  // electronsLoose, electronsMedium, electronsTight   are list of electrons that passed respective efficiency and reconstruction cuts
-  // muonsCombinedPlus, muonsCombined                  as above for muons
-  // photonsMedium                                     as above for photons
-  // jets are all reconstructed jets                   as above for jets. Note that electrons are most certainly also reconstructed as a jet -> overlap removal do avoid double counting necessary!
-  // tracks, towers                                    calorimeter and tracker information. Usually not needed.
-  // missingET                                         rec missing ET EXCLUDING muons.
-
-  
-  // Here is a couple of useful functions and lines:  
-  //------------Phase Space Cuts (defined for jets, electronsXYZ, muonsXYZ, photonsXYZ)
-  // jets = filterPhaseSpace(jets, 20., -2.8, 2.8)  // The vector 'jets' only contains jets with pt >= 20 GeV and -2.8 < eta < 2.8. This function is applicable to other particles too (electronsMedium, ... ).
-  // jets = overlapRemoval(jets, electronsLoose, 0.2) Removes all jets for which there exists any electron in 'electronsLoose' with deltaR < 0.2.
-  // jets = overlapRemovel(jets, 0.2) If two jets overlap within deltaR < 0.2, only the harder jet is stored.
-  
-  //------------Isolation Checks (defined for electronsXYZ, muonsXYZ, photonsXYZ
-  //------------        For each object, if the user entered N isolation conditions, they can be
-  //------------        checked individually be the second argument (running from 0 to N-1).
-  // electronsMedium = filterIsolation(electronsMedium, 0)            Removes electrons that do not pass the first isolation condition entered into the AnalysisManager by the user
-  // std::vector<int> flags; flags.push_back(0); flags.push_back(2);
-  // electronsMedium = filterIsolation(electronsMedium, flags)        Same as above, but both the first and the third condition have to be fulfilled
-  // electronsMedium = filterIsolation(electronsMedium)               Same as above, but all conditions have to be fulfilled.
-  
-  //-----------Flavour Tag Checks (defined for jets only)
-  //----------          Tau tags "loose", "medium" or "tight" can be individually checked if the user enabled tau tagging in the AM.
-  //----------          For b-tags, if N working points have been defined, the ith condition can be tested by putting i-1 into the second argument (if there is only one, the argument can be omitted)
-  // if checkTauTag(jets[0], "tight") leadingJetIsTagged = True;
-  // if checkBTag(jets[0], 0) leadingJetIsBTagged = True;
-
-
-  //-----------Auxiliary Information
-  // - Always ensure that you don't access vectors out of bounds. E.g. 'if(jets[1]->PT > 150)' should rather be if (jets.size() > 1 && jets[1]->PT > 150). 
-  // - Use rand()/(RAND_MAX+1.) for random numbers between 0 and 1. The random seed is determined from system time or by the RandomSeed parameter in CheckMATE.
-  // - The 'return' statement will end this function for the current event and hence should be called whenever the current event is to be vetoed.
-  // - Many advanced kinematical functions like mT2 are implemented. Check the manual for more information.
-  // - If you need output to be stored in other files than the cutflow/signal files we provide, check the manual for how to do this conveniently.  
 
   missingET->addMuons(muonsCombined);  // Adds muons to missing ET. This should almost always be done which is why this line is not commented out.
+  
+  electronsLoose = filterPhaseSpace(electronsLoose, 4.5, -2.47, 2.47);
+  muonsLoose = filterPhaseSpace(muonsLoose, 4., -2.7, 2.7);
+  photonsMedium = filterPhaseSpace(photonsMedium, 20., -2.37, 2.37, true);
+  jets = filterPhaseSpace(jets, 20., -4.5, 4.5);
+  
+  photonsMedium = overlapRemoval(photonsMedium, electronsLoose, 0.4, "y");
+  photonsMedium = overlapRemoval(photonsMedium, muonsLoose, 0.4, "y");
+  jets = overlapRemoval(jets, electronsLoose, 0.2, "y");
+  electronsLoose = specialoverlap(electronsLoose, jets);
+  jets = overlapRemoval_muon_jet_tracks(jets, muonsLoose, 0.2, 2); 
+  muonsLoose = specialoverlap(muonsLoose, jets);
+  jets = overlapRemoval(jets, photonsMedium, 0.4, "y");
+  
+  std::vector<Jet*> sigjets = filterPhaseSpace(jets, 25., -4.5, 4.5);
+  
+  countCutflowEvent("00_all");
+  
+  double met = missingET->P4().Et();
+  
+  TLorentzVector pTmiss_soft = TLorentzVector(0., 0., 0., 0.);
+  for (std::vector<Track*>::iterator it=tracks.begin(); it!=tracks.end(); it++) {
+    bool used = false;
+    for (int i = 0; i < jets.size() && !used; i++)
+      for (int part = 0; part < jets[i]->Particles.GetEntries(); part++)
+        if ((*it)->PT > 0.4 && jets[i]->Particles.At(part) == (*it)->Particle) {
+	  used = true;
+	  break;
+	}
+    if (!used)  pTmiss_soft -= (*it)->P4();
+  }
+    
+  countCutflowEvent("01_pTH>75");  
+    
+  if (sigjets.size() < 2 or sigjets[0]->PT < 50. or sigjets[1]->PT < 40. or fabs(sigjets[0]->Eta - sigjets[1]->Eta) < 2.) return;  
+  countCutflowEvent("02_jets");  
+  
+  if (met < 110.) return;
+  countCutflowEvent("03_METtrigger");
+  
+  if ( muonsLoose.size() > 0 and muonsLoose[0]->PT > 30.) return;
+  if ( electronsLoose.size() > 0 and electronsLoose[0]->PT > 30.) return;
+  countCutflowEvent("04_signal_lep_veto");
+  
+  if (photonsMedium.size() > 0 ) return;
+  countCutflowEvent("05_photon_veto");
+  
+  if (sigjets.size() > 4 ) return;
+  countCutflowEvent("06_Njet<5");
+  
+  if ( sigjets.size() > 2) 
+     for (int i = 2; i < sigjets.size(); i++) { 
+        double ci = exp(-4./pow(sigjets[0]->Eta - sigjets[1]->Eta, 2) * pow(sigjets[i]->Eta - (sigjets[0]->Eta + sigjets[1]->Eta)/2., 2 ) );
+        if (ci > 0.6) return;
+     }
+  countCutflowEvent("07_Ci<0.6");
+  
+  double mjj = (sigjets[0]->P4() + sigjets[1]->P4()).M();
+  if ( sigjets.size() > 2) 
+     for (int i = 2; i < sigjets.size(); i++) { 
+        double m1 = (sigjets[0]->P4() + sigjets[i]->P4()).M();
+        double m2 = (sigjets[1]->P4() + sigjets[i]->P4()).M();
+        if (std::min(m1, m2)/mjj > 0.05) return;
+     }
+  countCutflowEvent("08_mi<0.05");  
+  
+  if (jets[0]->PT < 80.) return;
+  countCutflowEvent("09_pTj1>80");
+  
+  if (jets[1]->PT < 50.) return;
+  countCutflowEvent("10_pTj2>50");  
+  
+  int nb = 0;
+  for (int i = 0; i < sigjets.size(); i++) 
+    if ( fabs(sigjets[i]->Eta) < 2.5 && checkBTag(sigjets[i]) ) nb++;  
+  if (nb > 1) return;  
+  countCutflowEvent("11_Nb<2");  
+  
+  if ( muonsLoose.size() > 0 or electronsLoose.size() > 0) return;
+  countCutflowEvent("12_cand_lep_veto");  
+  
+  if (pTmiss_soft.Perp() > 20.) return;
+  countCutflowEvent("13_softMET<20");  
+  
+  if (met < 160.) return;
+  countCutflowEvent("14_MET>160");  
+  
+  TLorentzVector alljets = TLorentzVector(0., 0., 0., 0.);
+  for (int i = 0; i < sigjets.size(); i++)
+      alljets += sigjets[i]->P4();
+  if (alljets.Perp() < 140.) return;
+  countCutflowEvent("15_pTjets>140");
+
+  double dphijj = fabs(sigjets[0]->P4().DeltaPhi(sigjets[1]->P4()));
+  if ( dphijj > 2.) return;
+  countCutflowEvent("16_DPhijj<2");  
+  
+  if ( sigjets[0]->Eta * sigjets[1]->Eta > 0) return;
+  countCutflowEvent("17_eta1*eta2<0");  
+  
+  if ( fabs(sigjets[0]->Eta - sigjets[1]->Eta) < 3.8 ) return;
+  countCutflowEvent("17_Detajj>3.8");
+
+  if (mjj < 800.) return;
+  countCutflowEvent("18_mjj>800");  
+  
+  if (sigjets.size() > 2 and mjj < 1500.) return;
+  countCutflowEvent("19_mjj(3,4)>1500");  
+  
+  if (met < 200. and mjj < 1500.) return;
+  countCutflowEvent("19_mjj(met)>1500");  
+  
+  countSignalEvent("SR");
+  
+  if (met < 200. and sigjets.size() == 2 ) {
+      if (mjj  <  2000. ) countSignalEvent("Bin14");
+      else if (mjj  <  3500. ) countSignalEvent("Bin15");
+      else countSignalEvent("Bin16");
+  }
+  
+  if (met > 200. and sigjets.size() > 2 ) {
+      if (mjj  <  2000. ) countSignalEvent("Bin11");
+      else if (mjj  <  3500. ) countSignalEvent("Bin12");
+      else countSignalEvent("Bin13");
+  }      
+  
+  if (met > 200. and sigjets.size() == 2 ) {
+      if (mjj < 1000.) {
+          if (dphijj < 1.0) countSignalEvent("Bin01");
+          else countSignalEvent("Bin06");
+      }
+      else if (mjj < 1500.) {
+          if (dphijj < 1.0) countSignalEvent("Bin02");
+          else countSignalEvent("Bin07");
+      }      
+      else if (mjj < 2000.) {
+          if (dphijj < 1.0) countSignalEvent("Bin03");
+          else countSignalEvent("Bin08");
+      }
+      else if (mjj < 3500.) {
+          if (dphijj < 1.0) countSignalEvent("Bin04");
+          else countSignalEvent("Bin09");
+      }
+      else {
+          if (dphijj < 1.0) countSignalEvent("Bin05");
+          else countSignalEvent("Bin10");
+      }      
+  }
+  
+  return;
   
 }
 
 void Atlas_2202_07953::finalize() {
   // Whatever should be done after the run goes here
 }       
+
+
+template <class X, class Y>
+std::vector<X*> Atlas_2202_07953::specialoverlap(std::vector<X*> candidates, std::vector<Y*> neighbours) {
+      // If neighbours are empty, return candidates
+      if(neighbours.size() == 0)
+        return candidates;
+      std::vector<X*> passed_candidates;
+      // Loop over candidates
+      for(int i = 0; i < candidates.size(); i++) {
+        bool overlap = false;
+        // If a neighbour is too close, declare overlap, break and don't save candidate
+        for(int j = 0; j < neighbours.size(); j++) {
+          if (candidates[i]->P4().DeltaR(neighbours[j]->P4()) > 0.2 and candidates[i]->P4().DeltaR(neighbours[j]->P4()) < std::min(0.4, 0.04 + 10./candidates[i]->PT) ) {
+            overlap = true;
+            break;
+          }
+        }
+        if (!overlap)
+          passed_candidates.push_back(candidates[i]);
+      }
+      return passed_candidates;
+    }
+    
+bool Atlas_2202_07953::check_nTrack_jet(Jet* jet, std::vector<Track*> tracks, int nTracksMin) {
+  
+  int nTracks = 0;
+  for (std::vector<Track*>::iterator it=tracks.begin(); it!=tracks.end(); it++) 
+    for (int part = 0; part < jet->Particles.GetEntries(); part++)
+      if (jet->Particles.At(part) == (*it)->Particle && (*it)->PT > 0.5) nTracks++;
+
+    return nTracks > nTracksMin;
+}
+    
+std::vector<Jet*> Atlas_2202_07953::overlapRemoval_muon_jet_tracks(std::vector<Jet*> cand_jets, std::vector<Muon*> cand_muons, double deltaR, int nTracks){
+  
+  std::vector<Jet*> passed;
+  for (std::vector<Jet*>::iterator jet = cand_jets.begin(); jet != cand_jets.end(); jet++) {
+  
+    if (check_nTrack_jet(*jet, tracks, nTracks)) {
+      passed.push_back(*jet);
+      continue;
+    }
+    double dR = deltaR;
+    bool iso = true;
+        
+    for (std::vector<Muon*>::iterator mu=cand_muons.begin(); mu!=cand_muons.end(); mu++) 
+      if ((*jet)->P4().DeltaR((*mu)->P4()) < dR) {
+	iso = false;
+	break;
+      }
+        		    
+    if (iso) passed.push_back(*jet);
+  }
+  
+  return passed;
+}    
+    
+
+    
+static bool isolatePhoton(Photon* photon, std::vector<Tower*> towers, double maxET, double deltaR) {
+  
+    double ETsum = -photon->PT;
+    for (std::vector<Tower*>::iterator it = towers.begin(); it != towers.end(); it++) {
+        if ((*it)->P4().DeltaR(photon->P4()) > deltaR) 
+            continue;
+        
+        if ((*it)->ET > 0.1)  
+	  ETsum += (*it)->ET;
+    }
+    return ETsum < maxET;
+}
+
+std::vector<Photon*> Atlas_2202_07953::isolatePhotons(std::vector<Photon*> photons, double deltaR) {
+  
+    std::vector<Photon*> isolated;
+    for (std::vector<Photon*>::iterator it = photons.begin(); it != photons.end(); it++) 
+        if ( isolatePhoton(*it, towers, 2.45 + 0.022*((*it)->PT), deltaR) )  
+            isolated.push_back(*it);        
+    
+    return isolated;
+}
