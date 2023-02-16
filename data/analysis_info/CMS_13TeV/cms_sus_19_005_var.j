39d593418263f87985b5b37acbecbc0e7cde18a0
K. Rolbiecki <krolb@fuw.edu.pl>
2023-02-16 16:51:54 +0100

    Updated cms_sus_19_005

--- data/analysis_info/CMS_13TeV/cms_sus_19_005_var.j
+++ data/analysis_info/CMS_13TeV/cms_sus_19_005_var.j
@@ -5,21 +5,26 @@
   "cr": "n",
   "ecm": "13.0",
   "electron_iso_absorrel": [
+    "r",
     "r"
   ],
   "electron_iso_dR": [
-    "0.2"
+    "0.05",
+    "0.3"
   ],
   "electron_iso_ptmin": [
-    "0.1"
+    "1.0",
+    "1.0"
   ],
   "electron_iso_ptratiomax": [
+    "0.1",
     "0.2"
   ],
   "electron_iso_source": [
-    "c"
+    "t",
+    "t"
   ],
-  "electron_niso": 1,
+  "electron_niso": 2,
   "expectation_known": "y",
   "experiment": "cms13tev",
   "info_set": [
@@ -41,21 +46,26 @@
   ],
   "lumi": "137.0",
   "muon_iso_absorrel": [
+    "r",
     "r"
   ],
   "muon_iso_dR": [
-    "0.05"
+    "0.05",
+    "0.3"
   ],
   "muon_iso_ptmin": [
-    "0.5"
+    "1.0",
+    "1.0"
   ],
   "muon_iso_ptratiomax": [
+    "0.1",
     "0.2"
   ],
   "muon_iso_source": [
+    "t",
     "t"
   ],
-  "muon_niso": 1,
+  "muon_niso": 2,
   "photon_iso_absorrel": [
     "r"
   ],
--- tools/analysis/src/analyses/CMS_13TeV/cms_sus_19_005.cc
+++ tools/analysis/src/analyses/CMS_13TeV/cms_sus_19_005.cc
@@ -24,51 +24,412 @@ void Cms_sus_19_005::initialize() {
 }
 
 void Cms_sus_19_005::analyze() {
-  // Your eventwise analysis code goes here
-  // The following objects are always defined unless they are 'ignored' above. They form std::vector objects of the respective Delphes class type (except for Etmiss which is a single object)
-  // All std::vector members and etmiss have the common properties PT, Eta, Phi and P4() with the latter giving access to the full ROOT TLorentzVector.
-  // Within a std::vector, all members are ordered with highest pt coming first.
 
-  // electronsLoose, electronsMedium, electronsTight   are list of electrons that passed respective efficiency and reconstruction cuts
-  // muonsCombinedPlus, muonsCombined                  as above for muons
-  // photonsMedium                                     as above for photons
-  // jets are all reconstructed jets                   as above for jets. Note that electrons are most certainly also reconstructed as a jet -> overlap removal do avoid double counting necessary!
-  // tracks, towers                                    calorimeter and tracker information. Usually not needed.
-  // missingET                                         rec missing ET EXCLUDING muons.
 
+  //missingET->addMuons(muonsCombined);  // Adds muons to missing ET. This should almost always be done which is why this line is not commented out.
   
-  // Here is a couple of useful functions and lines:  
-  //------------Phase Space Cuts (defined for jets, electronsXYZ, muonsXYZ, photonsXYZ)
-  // jets = filterPhaseSpace(jets, 20., -2.8, 2.8)  // The vector 'jets' only contains jets with pt >= 20 GeV and -2.8 < eta < 2.8. This function is applicable to other particles too (electronsMedium, ... ).
-  // jets = overlapRemoval(jets, electronsLoose, 0.2) Removes all jets for which there exists any electron in 'electronsLoose' with deltaR < 0.2.
-  // jets = overlapRemovel(jets, 0.2) If two jets overlap within deltaR < 0.2, only the harder jet is stored.
+  electronsLoose = filterPhaseSpace(electronsLoose, 5., -2.4, 2.4);
+  std::vector<Electron*> electronsPF = filterPhaseSpace(electronsLoose, 10., -2.4, 2.4, false, true);   // =>   pT < 10
+  electronsMedium = filterPhaseSpace(electronsMedium, 10., -2.4, 2.4);
+  muonsCombined = filterPhaseSpace(muonsCombined, 5., -2.4, 2.4);
+  std::vector<Muon*> muonsPF = filterPhaseSpace(muonsCombined, 10., -2.4, 2.4, false, true);
+  muonsCombined = filterPhaseSpace(muonsCombined, 10., -2.4, 2.4);
   
-  //------------Isolation Checks (defined for electronsXYZ, muonsXYZ, photonsXYZ
-  //------------        For each object, if the user entered N isolation conditions, they can be
-  //------------        checked individually be the second argument (running from 0 to N-1).
-  // electronsMedium = filterIsolation(electronsMedium, 0)            Removes electrons that do not pass the first isolation condition entered into the AnalysisManager by the user
-  // std::vector<int> flags; flags.push_back(0); flags.push_back(2);
-  // electronsMedium = filterIsolation(electronsMedium, flags)        Same as above, but both the first and the third condition have to be fulfilled
-  // electronsMedium = filterIsolation(electronsMedium)               Same as above, but all conditions have to be fulfilled.
+  electronsPF = filterIsolation( electronsPF, 0);
+  electronsMedium = Isolate_leptons_with_variable_track_isolation_cone_CMS(electronsMedium, 0.2, 0.05, 10., 0.1);
+  muonsPF = filterIsolation( muonsPF, 0);
+  muonsCombined = Isolate_leptons_with_variable_track_isolation_cone_CMS(muonsCombined, 0.2, 0.05, 10., 0.2);
   
-  //-----------Flavour Tag Checks (defined for jets only)
-  //----------          Tau tags "loose", "medium" or "tight" can be individually checked if the user enabled tau tagging in the AM.
-  //----------          For b-tags, if N working points have been defined, the ith condition can be tested by putting i-1 into the second argument (if there is only one, the argument can be omitted)
-  // if checkTauTag(jets[0], "tight") leadingJetIsTagged = True;
-  // if checkBTag(jets[0], 0) leadingJetIsBTagged = True;
-
-
-  //-----------Auxiliary Information
-  // - Always ensure that you don't access vectors out of bounds. E.g. 'if(jets[1]->PT > 150)' should rather be if (jets.size() > 1 && jets[1]->PT > 150). 
-  // - Use rand()/(RAND_MAX+1.) for random numbers between 0 and 1. The random seed is determined from system time or by the RandomSeed parameter in CheckMATE.
-  // - The 'return' statement will end this function for the current event and hence should be called whenever the current event is to be vetoed.
-  // - Many advanced kinematical functions like mT2 are implemented. Check the manual for more information.
-  // - If you need output to be stored in other files than the cutflow/signal files we provide, check the manual for how to do this conveniently.  
-
-  missingET->addMuons(muonsCombined);  // Adds muons to missing ET. This should almost always be done which is why this line is not commented out.
+  countCutflowEvent("00_all");
+  if ( muonsCombined.size() + electronsMedium.size() ) return;
+  countCutflowEvent("01_lep_veto");
   
+  for (int i = 0; i < electronsPF.size(); i++) 
+    if ( mT(electronsPF[i]->P4(), missingET->P4() ) < 100. ) return;
+    
+  for (int i = 0; i < muonsPF.size(); i++) 
+    if ( mT(muonsPF[i]->P4(), missingET->P4() ) < 100. ) return;  
+    
+  countCutflowEvent("02_softlep_veto");
+  
+  jets = filterPhaseSpace(jets, 20., -4.7, 4.7);
+  if (dPhi(jets, 0) < 0.3) return;
+  countCutflowEvent("03_dPhi_min");
+  
+  std::vector<Jet*> bjets;
+  std::vector<Jet*> lightjets;
+  for (int i = 0; i < jets.size();  i++) 
+    if ( fabs(jets[i]->Eta) < 2.4 and checkBTag(jets[i]) ) bjets.push_back(jets[i]);
+    else lightjets.push_back(jets[i]);
+  
+  lightjets = filterPhaseSpace(lightjets, 30., -2.4, 2.4);  
+  
+  int Nj = lightjets.size();
+  int Nb = bjets.size();
+  if ( !Nj ) return;
+  countCutflowEvent("04_Nj>=1");
+  
+  double  HT = 0.;
+  TLorentzVector HTmiss = TLorentzVector(0.,0.,0.,0.);
+  for (int i = 0; i < lightjets.size(); i++) {
+    HT += lightjets[i]->PT;
+    HTmiss -= lightjets[i]->P4();
+  }
+  for (int i = 0; i < bjets.size(); i++) {
+    HT += bjets[i]->PT;
+    HTmiss -= bjets[i]->P4();
+  }
+  
+  double MET = missingET->P4().Perp();
+  if ( MET < 30.) return;
+  countCutflowEvent("05_MET<30");
+  
+  if (  (missingET->P4() - HTmiss).Perp()/MET < 0.5) return;
+  countCutflowEvent("06_MET-HTmiss");
+  
+  if ( HT < 250.) return;
+  countCutflowEvent("07_HT>250");
+  
+  if ( (HT < 1200. or Nj == 1) and MET < 250.) return;
+  countCutflowEvent("08_MET>250");
+  
+  if (Nj > 1 ) {
+    std::vector<Jet*> sigjets = lightjets;
+    sigjets.insert(sigjets.end(), bjets.begin(), bjets.end());
+    std::sort(sigjets.begin(), sigjets.end(), sortByPT ); 
+  
+    std::vector<LundCluster> clusters = cluster_jets(sigjets);
+    
+    /*for (int j = 0; j < 2; j++) {
+      cout << "Cluster "+ std::to_string(j) << endl;
+      cout << "Momentum: " << clusters[j].P4().E() << ", " << clusters[j].P4().X() << ", " << clusters[j].P4().Y() << ", " << clusters[j].P4().Z() << endl;
+      cout << "Momentum: " << clusters[j].P4().Perp()  << endl;
+      for (int i = 0; i < clusters[j].constituents.size(); i++) {
+        cout << "Jet     : " << clusters[j].constituents[i]->P4().E() << ", " << clusters[j].constituents[i]->P4().X() << ", " << clusters[j].constituents[i]->P4().Y() << ", " << clusters[j].constituents[i]->P4().Z() << ", d1: " << clusters[0].Lund_dist(clusters[j].constituents[i]) << ", d2: " << clusters[1].Lund_dist(clusters[j].constituents[i]) << endl; 
+      }
+    } */
+    
+    std::string labmt2;
+    std::string labj;
+    std::string labb;
+    double MT2 = mT2(clusters[0].P4(), clusters[1].P4(), 0., missingET->P4());
+    
+    if (MT2 < 200.) return;
+    countCutflowEvent("09_MT2>200");
+    
+    if ( HT < 450.) {
+      if ( (Nb < 3 and Nj < 7) or (Nb == 0 and Nj > 6)) {
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else labmt2 = "400"; 
+        if (Nj < 4) labj = "2-3j_"; 
+        else if (Nj < 7) labj = "4-6j_";
+        else labj = "7j_";  
+        countSignalEvent(labj + std::to_string(Nb) + "b_250_" + labmt2);
+      }
+      else if ( Nb > 2 and Nj < 7) {
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else labmt2 = "400"; 
+        labj = "2-6j_" ; 
+        countSignalEvent(labj + std::to_string(Nb) + "b_250_" + labmt2);
+      }
+      else if ( Nb > 0 and Nj > 6) {
+        if (MT2 < 300.) labmt2 = "200"; 
+        else labmt2 = "300"; 
+        labj = "7j_";
+        labb = std::to_string(std::min(Nb, 3)); 
+        countSignalEvent(labj + labb + "b_250_" + labmt2);
+      }      
+    }
+    else if ( HT < 575.) {
+      if ( Nb < 3 and Nj < 7 ) {
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 500.) labmt2 = "400";
+        else labmt2 = "500";  
+        if (Nj < 4) labj = "2-3j_"; 
+        else labj = "4-6j_";
+        countSignalEvent(labj + std::to_string(Nb) + "b_450_" + labmt2);
+      }      
+      else if ( Nb > 2 and Nj < 7 ) {
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 500.) labmt2 = "400";
+        else labmt2 = "500";  
+        labj = "2-6j_"; 
+        labb = "3"; 
+        countSignalEvent(labj + labb + "b_450_" + labmt2);
+      } 
+      else if ( Nj > 6 ) {
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else labmt2 = "400"  ;
+        labj = "7j_"; 
+        labb = std::to_string(std::min(Nb, 3)); 
+        countSignalEvent(labj + labb + "b_450_" + labmt2);
+      }       
+    }
+    else if ( HT < 1200.) {
+      if (Nj < 4 and Nb == 0){
+        labmt2 = std::to_string(std::min(int(MT2/100.), 11)) + "00" ;
+        cout << MT2 << "    "  << int(std::floor(MT2/100));
+        countSignalEvent("2-3j_0b_575_" + labmt2);
+      }
+      else if (Nj < 4 and Nb == 1){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 600.) labmt2 = "400";
+        else if (MT2 < 800.) labmt2 = "600";
+        else if (MT2 < 1000.) labmt2 = "800";
+        else labmt2 = "1000";  
+        countSignalEvent("2-3j_1b_575_" + labmt2);
+      }      
+      else if (Nj < 4 and Nb == 2){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 600.) labmt2 = "400";
+        else if (MT2 < 800.) labmt2 = "600";
+        else labmt2 = "800";  
+        countSignalEvent("2-3j_2b_575_" + labmt2);
+      }  
+      else if (Nj < 7 and Nb == 3){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 600.) labmt2 = "400";
+        else if (MT2 < 800.) labmt2 = "600";
+        else labmt2 = "800";  
+        countSignalEvent("2-6j_3b_575_" + labmt2);
+      }
+      else if (Nj < 7 and Nb == 0){
+        labmt2 = std::to_string(std::min(int(MT2/100.), 11)) + "00" ;
+        cout << MT2 << "    "  << int(std::floor(MT2/100)) << endl;
+        countSignalEvent("4-6j_0b_575_" + labmt2);
+      }    
+      else if (Nj < 7 and Nb == 1){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 600.) labmt2 = "400";
+        else if (MT2 < 800.) labmt2 = "600";
+        else if (MT2 < 1000.) labmt2 = "800";
+        else labmt2 = "1000";  
+        countSignalEvent("4-6j_1b_575_" + labmt2);
+      }
+      else if (Nj < 7 and Nb == 2){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 600.) labmt2 = "400";
+        else if (MT2 < 800.) labmt2 = "600";
+        else labmt2 = "800";  
+        countSignalEvent("4-6j_2b_575_" + labmt2);
+      }        
+     else if (Nj < 10 and Nb < 3){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 600.) labmt2 = "400";
+        else if (MT2 < 800.) labmt2 = "600";
+        else labmt2 = "800";  
+        countSignalEvent("7-9j_"+ std::to_string(Nb)+"b_575_" + labmt2);
+      }    
+     else if (Nj < 10 and Nb == 3){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else if (MT2 < 600.) labmt2 = "400";
+        else labmt2 = "600";  
+        countSignalEvent("7-9j_"+ std::to_string(Nb)+"b_575_" + labmt2);
+      }   
+     else if (Nj < 10 and Nb > 3){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 400.) labmt2 = "300"; 
+        else labmt2 = "400";  
+        countSignalEvent("7-9j_4b_575_" + labmt2);
+      } 
+     else if (Nj >= 10 and Nb < 3){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else if (MT2 < 500.) labmt2 = "300"; 
+        else labmt2 = "500";  
+        countSignalEvent("7-9j_"+ std::to_string(Nb)+"b_575_" + labmt2);
+      }    
+     else if (Nj >= 10 and Nb == 3){
+        if (MT2 < 300.) labmt2 = "200"; 
+        else labmt2 = "300";  
+        countSignalEvent("7-9j_"+ std::to_string(Nb)+"b_575_" + labmt2);
+      }
+      else if (Nj >= 10 and Nb > 3) countSignalEvent("7-9j_4b_575_200");
+    }
+    else if ( HT < 1500.) {
+      if ( Nj < 4 and Nb < 2){
+        labmt2 = std::to_string(std::min(2*int(MT2/200.), 12)) + "00" ;
+        cout << MT2 << "    "  << 2*int(std::floor(MT2/200)) << endl;
+        countSignalEvent("2-3j_"+std::to_string(Nb)+"b_1200_" + labmt2);
+      }
+      else if ( Nj < 7 and Nb < 2){
+        labmt2 = std::to_string(std::min(2*int(MT2/200.), 12)) + "00" ;
+        cout << MT2 << "    "  << 2*int(std::floor(MT2/200)) << endl;
+        countSignalEvent("4-6j_"+std::to_string(Nb)+"b_1200_" + labmt2);
+      }  
+      else if ( Nj < 4 and Nb == 2){
+        labmt2 = std::to_string(std::min(2*int(MT2/200.), 10)) + "00" ;
+        cout << MT2 << "    "  << 2*int(std::floor(MT2/200)) << endl;
+        countSignalEvent("2-3j_"+std::to_string(Nb)+"b_1200_" + labmt2);
+      }  
+      else if ( Nj < 7 and Nb == 2){
+        labmt2 = std::to_string(std::min(2*int(MT2/200.), 10)) + "00" ;
+        cout << MT2 << "    "  << 2*int(std::floor(MT2/200)) << endl;
+        countSignalEvent("4-6j_"+std::to_string(Nb)+"b_1200_" + labmt2);
+      }
+      else if ( Nj < 7 and Nb >= 2){
+        labmt2 = std::to_string(std::min(2*int(MT2/200.), 6)) + "00" ;
+        cout << MT2 << "    "  << 2*int(std::floor(MT2/200)) << endl;
+        countSignalEvent("2-6j_3b_1200_" + labmt2);
+      }      
+      
+    }
+    
+  }
+  
+  return;
 }
 
 void Cms_sus_19_005::finalize() {
   // Whatever should be done after the run goes here
 }       
+
+
+template <class X>
+std::vector<X*> Cms_sus_19_005::Isolate_leptons_with_variable_track_isolation_cone_CMS(std::vector<X*> leptons, double dR_track_max, double dR_track_min, double pT_for_inverse_function_track, double pT_amount_track ) {
+      
+  std::vector<X*> filtered_leptons;
+  for(int i = 0; i < leptons.size(); i++) {
+    double dR_track = 0.;
+    double sumPT = 0.;
+    dR_track = std::min(dR_track_max, std::max(pT_for_inverse_function_track/leptons[i]->PT, dR_track_min));
+        
+    for (int t = 0; t < tracks.size(); t++) {
+      Track* neighbour = tracks[t];
+
+	  // Ignore the lepton's track itself
+      if(neighbour->Particle == leptons[i]->Particle) continue;
+      if(neighbour->PT < 1.) continue;
+      if (neighbour->P4().DeltaR(leptons[i]->P4()) > dR_track) continue;
+      sumPT += neighbour->PT;
+    }
+    
+    if( (leptons[i]->PT)*pT_amount_track > sumPT) filtered_leptons.push_back(leptons[i]);
+  }
+    
+    return filtered_leptons;
+}
+
+double Cms_sus_19_005::dPhi(std::vector<Jet*> jets, int j) {
+
+  int kmax = 0; int kmin = 0;
+  if ( !j ) { 
+    kmax = std::min(int(jets.size()), 4); 
+    kmin = 0;     
+  }
+  else { 
+    kmax = jets.size(); 
+    kmin = 3;    
+  }
+  double dphimin = 10.;
+
+  for (int k = kmin; k < kmax; k++)
+      dphimin = std::min(fabs(jets[k]->P4().DeltaPhi( missingET->P4() )), dphimin);
+     
+  return dphimin;
+}    
+
+
+std::vector<LundCluster> Cms_sus_19_005::cluster_jets(std::vector<Jet*> input) {
+  
+  //find seeds
+  int m = -1; int n = -1;
+  double minv = 0.;
+  for (int i = 0; i < input.size()-1 ; i++)
+    for(int j = i; j < input.size(); j++) {
+      double tmp = (input[i]->P4() + input[j]->P4()).M();
+      if ( tmp > minv ) {
+        minv = tmp;
+        n = i;
+        m = j;
+      }
+    }
+    
+  LundCluster cluster1 = LundCluster(input[n]);  
+  LundCluster cluster2 = LundCluster(input[m]);  
+  
+  input.erase(input.begin() + m); // m > n
+  input.erase(input.begin() + n);
+  
+  for (int i = 0; i < input.size() ; i++)  
+    if (cluster1.Lund_dist(input[i]) < cluster2.Lund_dist(input[i]) ) cluster1.add_jet(input[i]);
+    else cluster2.add_jet(input[i]);
+  
+  bool stable = 0; int k = 0; 
+  while (!stable and k < 20)  {
+    stable = 1;
+    k++;
+    for (int i = 0; i < cluster1.constituents.size(); i++) 
+      if (cluster1.Lund_dist(cluster1.constituents[i]) > cluster2.Lund_dist(cluster1.constituents[i]) ) {
+        cluster1.remove_jet(cluster1.constituents[i]);
+        cluster2.add_jet(cluster1.constituents[i]);
+        stable = 0;
+      }
+    for (int i = 0; i < cluster2.constituents.size(); i++) 
+      if (cluster2.Lund_dist(cluster2.constituents[i]) > cluster1.Lund_dist(cluster2.constituents[i]) ) {
+        cluster2.remove_jet(cluster2.constituents[i]);
+        cluster1.add_jet(cluster2.constituents[i]);
+        stable = 0;
+      }         
+  }
+  
+  std::vector<LundCluster> clusters;
+  clusters.push_back(cluster1);
+  clusters.push_back(cluster2);
+  
+  if ( k == 20) {
+    for (int j = 0; j < 2; j++) {
+      cout << "Cluster "+ std::to_string(j) << endl;
+      cout << "Momentum: " << clusters[j].P4().E() << ", " << clusters[j].P4().X() << ", " << clusters[j].P4().Y() << ", " << clusters[j].P4().Z() << endl;
+      cout << "Momentum: " << clusters[j].P4().Perp()  << endl;
+      for (int i = 0; i < clusters[j].constituents.size(); i++) {
+        cout << "Jet     : " << clusters[j].constituents[i]->P4().E() << ", " << clusters[j].constituents[i]->P4().X() << ", " << clusters[j].constituents[i]->P4().Y() << ", " << clusters[j].constituents[i]->P4().Z() << ", d1: " << clusters[0].Lund_dist(clusters[j].constituents[i]) << ", d2: " << clusters[1].Lund_dist(clusters[j].constituents[i]) << endl; 
+      }
+    }    
+    
+  }
+  
+  return clusters;
+  
+}
+
+
+LundCluster::LundCluster(Jet* jet) {
+  
+  constituents.push_back(jet);
+  momentum = jet->P4();
+  
+}
+
+void LundCluster::add_jet(Jet* jet) {
+  
+  constituents.push_back(jet);
+  std::sort(constituents.begin(), constituents.end(), sortByPT ); 
+  momentum += jet->P4();
+  
+}
+
+void LundCluster::remove_jet(Jet* jet) {
+  
+  if (constituents.size() > 1) 
+    for (int i = 0; i < constituents.size(); i++)
+      if (jet == constituents[i]) { 
+        constituents.erase(constituents.begin() + i);
+        momentum -= jet->P4();
+        return;
+      }
+  return;
+}
+
+  
